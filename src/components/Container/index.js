6c10bb9f2c560bac93ac857d87a93aa4910659fb
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-19 00:50:07 +0100

    Add container component

---
+++ src/components/Container/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Wrapper, } from './ContainerElements';
+
+
+const Container = ({ children }) => {
+
+    return (
+        <Wrapper>
+            {children}
+        </Wrapper>
+    );
+};
+
+export default Container;
