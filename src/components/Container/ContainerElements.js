d851d9226bb619990861b6b07f751420f7d78bcb
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-19 00:50:29 +0100

    Add style file for container elements

---
+++ src/components/Container/ContainerElements.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 40px;
+    background: #fff;
+    border: 1px solid #E1E1E1;
+    border-radius: 3px;
+`;
+
+
+
