73c2d4142af33e0c9420306278b67054b9e8a068
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-19 00:51:46 +0100

    Add style file for container navbar

---
+++ src/components/Container/Navbar/NavbarElements.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const NavbarContainer = styled.div`
+    width: 100%;
+    margin: 20px 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const Logo = styled.img`
+    height: 40px;
+`;
+
+export const Basket = styled.img`
+    height: 30px;
+    cursor: pointer;
+`;
