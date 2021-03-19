04524915711308ddc56adb8feb77dded9343ff32
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-19 15:25:02 +0100

    Add action handler for basket icon

--- src/components/Container/Navbar/index.js
+++ src/components/Container/Navbar/index.js
@@ -3,11 +3,11 @@ import { NavbarContainer, Logo, Basket } from './NavbarElements';
 import logo from '../../../utils/images/logo.svg';
 import basket from '../../../utils/images/basket.svg';
 
-const Navbar = () => {
+const Navbar = ({action}) => {
     return (
         <NavbarContainer>
             <Logo src={logo} alt="10A" />
-            <Basket src={basket} alt="basket"/>
+            <Basket src={basket} alt="basket" onClick={action}/>
         </NavbarContainer>
     );
 };
