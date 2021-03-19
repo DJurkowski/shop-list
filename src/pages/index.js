5f2b00f3b098f9bd24372e5d8e66d87febba5237
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-19 17:26:08 +0100

    Add context providers and initial context values

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,25 @@ import Container from '../components/Container';
 import ContainerNavbar from '../components/Container/Navbar';
 import OrderSidebar from '../components/OrderSidebar';
 import List from '../components/ShopList';
+import { Context } from '../utils/context/Context';
 
 const Home = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [orders, setOrders] = useState([]);
 
     const handleSidebar = (value) => {
         setIsOpen(value);
     };
 
     return (
-        <Container>
-            <OrderSidebar isOpen={isOpen} closeAction={handleSidebar}/>
-            <ContainerNavbar action={handleSidebar}/>
-            <List />
-        </Container>
+        <Context.Provider value={[orders, setOrders]}>
+            <Container>
+                <OrderSidebar isOpen={isOpen} closeAction={handleSidebar}/>
+                <ContainerNavbar action={handleSidebar}/>
+                <List />
+            </Container>
+        </Context.Provider>
     )
 }
 
