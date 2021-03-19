5cafc90c0597aebb518f4b730f274650df6ed50e
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-03-19 17:56:01 +0100

    Add styling for notification element

--- src/components/Container/Navbar/NavbarElements.js
+++ src/components/Container/Navbar/NavbarElements.js
@@ -11,7 +11,26 @@ export const Logo = styled.img`
     height: 40px;
 `;
 
+export const IconWrapper = styled.div`
+    position: relative;
+`;
+
 export const Basket = styled.img`
     height: 30px;
     cursor: pointer;
+`;
+
+export const Notification = styled.div`
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20px;
+    height: 20px;
+    font-size: 13px;
+    border-radius: 50%;
+    color: #fff;
+    background: #00BE97;
+    bottom: -7px;
+    right: -7px;
 `;
